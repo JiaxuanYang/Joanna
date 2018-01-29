26ba7c7f983e968a14e4bddc1d4cb05daaf9734b
Joanna <yangjiaxuan1998@gmail.com>
2018-01-29 16:20:34 -0500

    Update first.c

--- first.c
+++ first.c
@@ -12,8 +12,8 @@
         struct _Node * next;};
         typedef struct _Node Node;
 
-	
-	Node *newNode(int data){
+	//create new node
+	Node *CreatenewNode(int data){
 		Node* newNode  = (Node*)malloc(100*sizeof(Node));
 		if(newNode==NULL){printf("error\n");return NULL;}
 		newNode -> data = data;
@@ -24,7 +24,6 @@
 	
 static Node* insert(Node *head,  int data);
 static Node* delete(Node *head, int data);
-//static void freeNode(Node *head);
 
 	int main(int argc, char *argv[]){
 	  
@@ -34,7 +33,7 @@ static Node* delete(Node *head, int data);
 	printf("error\n");
 	return 0;
 	}
-	// check if exist
+	// check if file does exist
 
 
 	fseek(file,0,SEEK_END);
@@ -45,20 +44,20 @@ static Node* delete(Node *head, int data);
 	return 0; 
 	}
 	rewind(file);
-	// check if empty
+	// check if file is empty
 
 	Node *head = NULL;	
 	
 	char *str1 = (char *)malloc(100*sizeof(char));
 	if(str1==NULL){printf("error\n");return 0;}
 	char ch1[1],ch2[1];
-	ch1[0] = 'i';
-	ch2[0] = 'd'; 
+	ch1[0] = 'i';	//instruction - insert
+	ch2[0] = 'd'; 	//iinstruction - delete
 	int str2;
-	//int i = 0;
+	//begin scan file, inputs are in format "[char][tab][int]"
 	while(fscanf(file, "%s\t%d", str1, &str2) != EOF){
 		
-	
+	//return error for wrong input format
 	if((str1[0] != ch1[0] && str1[0] != ch2[0])&&(!isdigit(str2))){
 		printf("error\n");
 		return 0;}
@@ -80,15 +79,17 @@ static Node* delete(Node *head, int data);
 	head = delete(head, str2);}	
 }
 ////////////////////////////////////////////////////////////////////////////////
-	//tmp3 = (Node *) malloc(sizeof(Node));
+	
 
 	Node *tmp3 = head;
 	int count = 0;
 
+	//empty list
 	if(tmp3 == NULL){
 	printf("0\n");
 	return 0;}
 
+	//find lenthg of list
 	while(tmp3 != NULL){
 	count++; 
 	tmp3 = tmp3 -> next;}
@@ -96,7 +97,7 @@ static Node* delete(Node *head, int data);
 	printf("%d\n",count); 
 
 	tmp3 = head;
-	while(tmp3 != NULL){
+	while(tmp3 != NULL){	//print list
         printf("%d\t",tmp3 -> data);  
         tmp3 = tmp3 -> next;}
 	printf("\n");
@@ -104,17 +105,14 @@ static Node* delete(Node *head, int data);
 
 
 
-
-	//free(head);
-        free(str1); 
-       // free(curr);
-	//freeNode(head);	
+        free(str1); 	
 	fclose(file);
 
 	return 0;
 }
 //////////////////////////////////////////////////////
-/*	void freeNode(Node* head){
+//free whole list
+void freeNode(Node* head){
 
 	Node *tmp = head;
 	while( head != NULL){
@@ -124,20 +122,20 @@ static Node* delete(Node *head, int data);
 	}
 	
 	}
-*/
+
 ///////////////////////////////////////////////////////////
 
  Node* insert(Node *root, int key){
 
-        if (root == NULL) return newNode(key);
+        if (root == NULL) return CreatenewNode(key);
 
 
     Node *tmp = root;
     Node *prev;   
-    Node *curr = newNode(key);
+    Node *curr = CreatenewNode(key);
 
-
-    if(curr->data < tmp->data){
+//if new node is smallest, then change head
+    if(curr->data < root->data){
         curr->next = root;   
         root = curr;
     return root;}                      
@@ -193,7 +191,6 @@ Node*delete(Node*head, int data){
 		if(tmp2 -> data == data && tmp2 -> next == NULL){
 		prv -> next = NULL;
 		free(tmp2);
-		//tmp2 -> next = NULL;
 		return head;}
 
 		prv = tmp2;
@@ -208,34 +205,3 @@ return head;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////
-/*
-	void delete(Node *head, int data){
-
-	if(head == NULL){return NULL;}
-
-	 if(head != NULL){
-
-                        //tmp2 = (Node *) malloc(sizeof(Node));
-                        Node * tmp2 = head;
-                        Node *next = NULL;
-                while(tmp2 -> next != NULL){
-                         if(tmp2 == head && tmp2 -> data == data){
-                        head = tmp2 -> next;
-                        }
-
-                        if(tmp2 -> next -> data == data && tmp2 -> next -> next != NULL){
-                        next = tmp2 -> next;
-                        tmp2 -> next = tmp2 -> next -> next;
-                        next -> next = NULL;}
-
-                        if(tmp2 -> next -> data == data && tmp2 -> next -> next == NULL){
-                        tmp2 -> next = NULL;
-                        break;}
-
-                        tmp2 = tmp2 -> next;
-                        next = tmp2 -> next;}
-}
-
-return;
-        }
-*/
